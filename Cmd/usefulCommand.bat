b5bd1921a5670045b2c9d919d84d18e38727b674
kenji-getpowered <mikael.krok@gmail.com>
2016-08-09 15:25:08 +0200

    Update usefulCommand.bat

--- Cmd/usefulCommand.bat
+++ Cmd/usefulCommand.bat
@@ -12,3 +12,11 @@ REM Redirect errors and output to a single file
 cmd > file 2>&1       
 REM Redirect output and errors to separate files 
 cmd > fileA 2> fileB  
+
+
+REM doskey
+REM launch with a doskey parameter file cmd.exe /K %PATH%\doskey.cmd
+
+DOSKEY ll=dir
+REM pass the parameters to the command
+DOSKEY gr=gradle $*
